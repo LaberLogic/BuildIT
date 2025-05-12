1b208c7c87837eee68974744e52179833646cfec
Jonas Labermeier <j.labermeier@lavego.de>
2025-05-12 19:20:53 +0200

    add prettier setup

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+export default defineConfig([
+  globalIgnores(["**/node_modules/", "**/generated/"]),
+  {
+    files: ["**/*.{js,mjs,cjs,ts,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,vue}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  tseslint.configs.recommended,
+  pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+
+  // Target only TypeScript files in the entire monorepo
+  {
+    files: ["**/*.ts"], // This targets all TypeScript files in the repo
+    languageOptions: {
+      parserOptions: { parser: tseslint.parser }, // Use the TypeScript parser
+    },
+  },
+  eslintConfigPrettier,
+
+]);
