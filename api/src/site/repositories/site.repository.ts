fb488980b6941d50dada433e9dc3b4c9e118d65c
Jonas Labermeier <j.labermeier@lavego.de>
2025-05-25 16:56:07 +0200

    add service tests

--- api/src/site/repositories/site.repository.ts
+++ api/src/site/repositories/site.repository.ts
@@ -1,12 +1,40 @@
 import { PrismaClient, Prisma } from "@prisma/prisma";
+import { SiteResponseDto } from "@src/schemas/siteSchema";
 import { ChainedError } from "@utils/chainedError";
 import { prismaErrorCodeToHttpStatus } from "@utils/errorCodeMapper";
 import { ResultAsync } from "neverthrow";
 const prisma = new PrismaClient();
 
-export const createSite = (data: Prisma.SiteCreateInput) => {
+const siteSelect = {
+  id: true,
+  name: true,
+  address: {
+    select: {
+      streetNumber: true,
+      street: true,
+      city: true,
+      country: true,
+      postalCode: true,
+    },
+  },
+  companyId: true,
+  startDate: true,
+  endDate: true,
+  notes: true,
+  assignments: {
+    select: {
+      userId: true,
+      firstName: true,
+      lastName: true,
+    },
+  },
+};
+
+export const createSite = (
+  data: Prisma.SiteCreateInput,
+): ResultAsync<SiteResponseDto, ChainedError> => {
   return ResultAsync.fromPromise(
-    prisma.site.create({ data }),
+    prisma.site.create({ data, select: siteSelect }),
     (e) => new ChainedError(e, prismaErrorCodeToHttpStatus(e)),
   );
 };
@@ -16,7 +44,25 @@ export const updateSite = (
   data: Prisma.SiteUpdateInput,
 ) => {
   return ResultAsync.fromPromise(
-    prisma.site.update({ where, data }),
+    prisma.site.update({ where, data, select: siteSelect }),
+    (e) => new ChainedError(e, prismaErrorCodeToHttpStatus(e)),
+  );
+};
+
+export const getSites = (
+  where: Prisma.SiteWhereInput,
+): ResultAsync<SiteResponseDto[], ChainedError> => {
+  return ResultAsync.fromPromise(
+    prisma.site.findMany({ where, select: siteSelect }),
+    (e) => new ChainedError(e, prismaErrorCodeToHttpStatus(e)),
+  );
+};
+
+export const getSite = (
+  where: Prisma.SiteWhereUniqueInput,
+): ResultAsync<SiteResponseDto, ChainedError> => {
+  return ResultAsync.fromPromise(
+    prisma.site.findUniqueOrThrow({ where, select: siteSelect }),
     (e) => new ChainedError(e, prismaErrorCodeToHttpStatus(e)),
   );
 };
