d39952bd8527dc50ad3f0955a8471446c29aa06b
Jonas Labermeier <j.labermeier@lavego.de>
2025-05-26 21:07:51 +0200

    add header and footer

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+  <app-header class="h-16" />
   <div class="flex justify-center">
     <div class="w-1/3 space-y-4 pb-4">
       <div
@@ -11,6 +12,7 @@
       </div>
     </div>
   </div>
+  <app-footer class="h-20" />
 </template>
 <script setup lang="ts">
 const sites = [
