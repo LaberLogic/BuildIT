ab62ece772dd4f3d4189be57bc286eabae8a87c8
Jonas Labermeier <j.labermeier@lavego.de>
2025-05-27 10:57:24 +0200

    basic token usage

---
+++ web/middleware/auth.ts
@@ -0,0 +1,7 @@
+export default defineNuxtRouteMiddleware((to) => {
+  const token = useCookie("token").value;
+
+  if (!token) {
+    return navigateTo("/auth/login");
+  } else navigateTo(to);
+});
