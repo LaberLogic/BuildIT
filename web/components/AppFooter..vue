b94ded9843a687f28d2672123db54964a434f3e3
Jonas Labermeier <j.labermeier@lavego.de>
2025-06-01 09:24:09 +0200

    add all update modals and user management for manager

--- web/components/AppFooter..vue
+++ web/components/AppFooter..vue
@@ -2,22 +2,33 @@
   <div
     class="fixed z-40 w-full bottom-0 bg-white border-t border-gray-200 px-4 flex items-center justify-center h-24"
   >
-    <div class="w-80 flex justify-between items-center h-full text-gray-600">
-      <NuxtLink v-for="item in navItems" :key="item.href" :to="item.href">
-        <div
-          class="hover:bg-gray-100 p-4 rounded-lg"
-          :class="{
-            'text-blue-400 bg-gray-50': isActiveRoute(item.href),
-          }"
+    <div
+      class="fixed z-40 w-full bottom-0 bg-white border-t border-gray-200 px-4 flex items-center justify-center h-24"
+    >
+      <div
+        class="flex justify-center items-center h-full text-gray-600 gap-x-12"
+      >
+        <NuxtLink
+          v-for="item in navItems"
+          :key="item.href"
+          :to="item.href"
+          class="flex flex-col justify-center items-center"
         >
-          <el-icon :size="25">
-            <component :is="item.icon"></component>
-          </el-icon>
-        </div>
-      </NuxtLink>
+          <div
+            class="hover:bg-gray-100 p-4 rounded-lg flex flex-col items-center"
+            :class="{
+              'text-blue-400 bg-gray-50': isActiveRoute(item.href),
+            }"
+          >
+            <el-icon :size="25">
+              <component :is="item.icon"></component>
+            </el-icon>
+            <span class="text-xs">{{ item.label }}</span>
+          </div>
+        </NuxtLink>
+      </div>
     </div>
   </div>
-  <div v-if="showBackButton" class="w-8"></div>
 </template>
 
 <script setup lang="ts">
@@ -30,7 +41,6 @@ import {
 
 import { useRoute } from "vue-router";
 
-const showBackButton = ref(true);
 const route = useRoute();
 
 const auth = useAuthStore();
@@ -54,7 +64,7 @@ const allNavItems = [
     icon: MessageBox,
     label: "Chat",
     badge: false,
-    roles: ["ADMIN", "MANAGER", "WORKER"],
+    roles: ["SUPER ADMIN"],
   },
   {
     href: "/profile",
