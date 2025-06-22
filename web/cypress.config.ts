5612c224ac37d62d7b692456360b6693b46e8f30
Jonas Labermeier <j.labermeier@lavego.de>
2025-06-22 11:12:25 +0200

    pipeline and jsdoc

--- web/cypress.config.ts
+++ web/cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     supportFile: "cypress/support/e2e.ts",
-    specPattern: "web/cypress/e2e/site/manager.site.cy.ts",
+    specPattern: "cypress/e2e/site/manager.site.cy.ts",
     retries: {
       runMode: 1,
       openMode: 1,
